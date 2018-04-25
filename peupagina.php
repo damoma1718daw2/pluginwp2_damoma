1a8bf781d1d9a9ea47be33ee5719e483afbb239c
damoma1718daw2 <32568778+damoma1718daw2@users.noreply.github.com>
2018-04-25 20:14:06 +0200

    Add files via upload

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Peu de pàgina per defecte per a noves entrades
+ * Plugin URI: https://github.com/damoma1718daw2/pluginwp2_damoma
+ * Description: Crea peus de pàgina per defecte per a noves entrades
+ * Version: 0.1
+ * Author: David Montilla Margalet
+ * Author URI: https://github.com/damoma1718daw2
+ * License: GNU GPLv3
+ * */
+ ?>
